731acc976c6082d4d1b68a904c3cae1fff6a6b37
Atsushi KAWASAKI <ak@xlix.org>
2023-03-10 19:28:41 +0900

    always semi style is better for me.

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,26 @@
-import * as core from '@actions/core'
-import * as github from '@actions/github'
-import {getSdk} from './graphql'
+import * as core from '@actions/core';
+import * as github from '@actions/github';
+import { getSdk } from './graphql';
 
 async function main(): Promise<void> {
   try {
-    const token = core.getInput('token')
+    const token = core.getInput('token');
 
-    const octokit = github.getOctokit(token)
+    const octokit = github.getOctokit(token);
 
-    const sdk = getSdk(octokit.graphql)
+    const sdk = getSdk(octokit.graphql);
 
     const milestone = await sdk.milestone({
       owner: github.context.payload.repository!.owner.login,
       repository: github.context.payload.repository!.name,
       number: 1
-    })
+    });
 
-    core.debug('milestone(1):')
-    core.debug(JSON.stringify(milestone, null, 2))
+    core.debug('milestone(1):');
+    core.debug(JSON.stringify(milestone, null, 2));
   } catch (error) {
-    if (error instanceof Error) core.setFailed(error.message)
+    if (error instanceof Error) core.setFailed(error.message);
   }
 }
 
-main()
+main();
