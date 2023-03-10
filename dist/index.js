59d09b58c220999055929859cb016fa45c108f65
Atsushi KAWASAKI <ak@xlix.org>
2023-03-10 17:01:13 +0900

    add builded dist/.

--- dist/index.js
+++ dist/index.js
@@ -44,6 +44,7 @@ const github = __importStar(__nccwpck_require__(5438));
 function run() {
     return __awaiter(this, void 0, void 0, function* () {
         try {
+            core.notice('some notice for you');
             core.debug('github.context:');
             core.debug(JSON.stringify(github.context, null, 2));
         }
