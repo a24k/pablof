e533ad74a102d0fbda337b1407deb92c7c83fbb0
Atsushi KAWASAKI <ak@xlix.org>
2023-03-12 00:04:27 +0900

    show result annotations.

--- dist/index.js
+++ dist/index.js
@@ -27,10 +27,33 @@ exports.collect = collect;
 /***/ }),
 
 /***/ 6253:
-/***/ (function(__unused_webpack_module, exports) {
+/***/ (function(__unused_webpack_module, exports, __nccwpck_require__) {
 
 "use strict";
 
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -42,6 +65,7 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 };
 Object.defineProperty(exports, "__esModule", ({ value: true }));
 exports.ActionInventory = void 0;
+const core = __importStar(__nccwpck_require__(2186));
 class ActionInventory {
     constructor() {
         this.items = [];
@@ -55,7 +79,20 @@ class ActionInventory {
     handleContext(context, sdk) {
         return __awaiter(this, void 0, void 0, function* () {
             for (const item of this.items) {
-                yield item.handleContext(context, sdk);
+                const result = yield item.handleContext(context, sdk);
+                result.match((res) => {
+                    switch (res.type) {
+                        case "Success":
+                            core.notice(res.message);
+                            break;
+                        case "Skip":
+                        default:
+                            core.notice("skip");
+                            break;
+                    }
+                }, (err) => {
+                    core.error(err.message);
+                });
             }
         });
     }
--- src/actions/inventory.ts
+++ src/actions/inventory.ts
@@ -1,5 +1,8 @@
+import * as core from "@actions/core";
+
 import { TriggerableAction } from "./";
 import type { Context, Sdk } from "./";
+import type { ActionOk, ActionErr } from "./result";
 
 export class ActionInventory {
   protected items: TriggerableAction[] = [];
@@ -14,7 +17,23 @@ export class ActionInventory {
 
   async handleContext(context: Context, sdk: Sdk): Promise<void> {
     for (const item of this.items) {
-      await item.handleContext(context, sdk);
+      const result = await item.handleContext(context, sdk);
+      result.match(
+        (res: ActionOk) => {
+          switch (res.type) {
+            case "Success":
+              core.notice(res.message);
+              break;
+            case "Skip":
+            default:
+              core.notice("skip");
+              break;
+          }
+        },
+        (err: ActionErr) => {
+          core.error(err.message);
+        }
+      );
     }
   }
 }
