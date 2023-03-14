5f0d883954597c917d34d1f17f37e247f36d49c6
Atsushi KAWASAKI <ak@xlix.org>
2023-03-14 20:04:18 +0900

    remove getSdk.

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,18 @@
 import * as core from "@actions/core";
 import * as github from "@actions/github";
 
-import { getSdk } from "./graphql";
+//import { getSdk } from "./graphql";
 //import { collect } from "./actions";
 
 async function main(): Promise<void> {
   try {
     const token = core.getInput("token");
     const octokit = github.getOctokit(token);
-    const sdk = getSdk(octokit.graphql);
+    //const sdk = getSdk(octokit.graphql);
 
     core.debug(`octokit = ${JSON.stringify(octokit, null, 2)}`);
     core.debug(`graphql = ${JSON.stringify(octokit.graphql, null, 2)}`);
-    core.debug(`sdk = ${JSON.stringify(sdk, null, 2)}`);
+    //core.debug(`sdk = ${JSON.stringify(sdk, null, 2)}`);
 
     //const inventory = collect();
 
