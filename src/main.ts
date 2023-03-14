5431d18f6fb82d880a51202861b631d7d1cfb6be
Atsushi KAWASAKI <ak@xlix.org>
2023-03-14 19:42:38 +0900

    don't collect inventory.

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import * as core from "@actions/core";
 import * as github from "@actions/github";
 
 import { getSdk } from "./graphql";
-import { collect } from "./actions";
+//import { collect } from "./actions";
 
 async function main(): Promise<void> {
   try {
@@ -14,9 +14,9 @@ async function main(): Promise<void> {
     core.debug(`graphql = ${JSON.stringify(octokit.graphql, null, 2)}`);
     core.debug(`sdk = ${JSON.stringify(sdk, null, 2)}`);
 
-    const inventory = collect();
+    //const inventory = collect();
 
-    await inventory.handleContext(github.context, sdk);
+    //await inventory.handleContext(github.context, sdk);
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message);
   }
